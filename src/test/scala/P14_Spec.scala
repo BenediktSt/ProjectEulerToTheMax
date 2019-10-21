271a13c71ee42f502edc1a9024c5671ad208226a
StefanGroth <stefan.groth@outlook.com>
2019-10-21 18:06:51 +0200

    test: fixed redundant string

--- src/test/scala/P14_Spec.scala
+++ src/test/scala/P14_Spec.scala
@@ -6,19 +6,19 @@ class P14_Spec extends FlatSpec {
     assert(9 === P14_LongestCollatzSequence.calculate(14))
   }
 
-  "P14_LongestCollatzSequence" should "calculate the correct result for statring number=5847" in {
+  it should "calculate the correct result for statring number=5847" in {
     assert(3711 === P14_LongestCollatzSequence.calculate(5847))
   }
 
-  "P14_LongestCollatzSequence" should "calculate the correct result for statring number=46500" in {
+  it should "calculate the correct result for statring number=46500" in {
     assert(35655 === P14_LongestCollatzSequence.calculate(46500))
   }
 
-  "P14_LongestCollatzSequence" should "calculate the correct result for statring number=54512" in {
+  it should "calculate the correct result for statring number=54512" in {
     assert(52527 === P14_LongestCollatzSequence.calculate(54512))
   }
 
-  "P14_LongestCollatzSequence" should "calculate the correct result for statring number=1000000" in {
+  it should "calculate the correct result for statring number=1000000" in {
     assert(837799 === P14_LongestCollatzSequence.calculate(1000000))
   }
 
